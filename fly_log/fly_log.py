2a22cd46356a61c1dc39e4bf61157f453ee1994b
flythinker <67495224@qq.com>
2021-11-04 16:45:52 +0800

    0.1.22

--- fly_log/fly_log.py
+++ fly_log/fly_log.py
@@ -4,12 +4,15 @@ import time
 
 
 old_print = print
+current_milli_time = lambda: int(round(time.time() * 1000))
 
 
 def debug_print(*p):
     current_frame = sys._getframe(1)
+
+    milli_tim_str = "%03d " % (current_milli_time() % 1000)
     code_info = " %s(%s:%s)" % (current_frame.f_code.co_name, os.path.basename(current_frame.f_code.co_filename), current_frame.f_lineno)
-    old_print(time.strftime("%Y-%m-%d %H:%M:%S") + code_info, *p)
+    old_print(time.strftime("%Y-%m-%d %H:%M:%S.")  + milli_tim_str + code_info, *p)
 
 
 
--- fly_log/fly_log_sample_dev_run.py
+++ fly_log/fly_log_sample_dev_run.py
@@ -1,10 +1,12 @@
 # from .fly_log import debug_print
 from fly_log import debug_print
+import time
+
+
 
 print = debug_print
 
 def dev_run1():
-    print("dev_run1")
     print("dev_run1")
 
 
